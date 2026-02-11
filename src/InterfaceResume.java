390473169ded6d1c96f8908bcb8c03fc4067c331
Your NameAbhinav <abhinavprasanth06@gmail.com>
2026-02-11 08:34:36 +0530

    added

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -13,7 +13,7 @@ class Teacher implements Resume {
     public void  biodata(){
 	System.out.println( "Name: "+Name);
 	System.out.println( "Qualification: "+Qualification);
-	System.out.println( "Experience: "+Experience+"years");
+	System.out.println( "Experience: "+Experience+" years");
     }
 }
 
